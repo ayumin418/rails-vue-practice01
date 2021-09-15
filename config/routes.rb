e8e7491f6889a3d8eb5e11b702ffc2f5a6daf26f
Ayumi Ito <56537575+cha-ayu@users.noreply.github.com>
2021-09-15 18:30:55 +0900

    Add json

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'home', to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, format: 'json' do
+    resources :memos, only: [:index]
+  end
 end
