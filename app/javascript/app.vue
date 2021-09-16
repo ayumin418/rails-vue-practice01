f8398e9a82cc5340f07e55997e83cd643c84b8da
Ayumi Ito <56537575+cha-ayu@users.noreply.github.com>
2021-09-16 15:34:26 +0900

    add axios

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <!-- <p>{{ message }}</p> -->
+    <ul>
+      <li v-for="memo in memos" :key="memo.id">
+        {{ memo.title }}: {{ memo.description }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      // message: "Hello Vue!"
+      memos: "memos"
+    }
+  },
+  mounted () {
+    this.setMemo();
+  },
+  methods: {
+    setMemo: function () {
+      axios.get('/api/memos')
+      .then(responce => (
+        this.memos = responce.data
+      ))
     }
   }
 }
